76bc5ec75373b925c7bda35f061937023f7dd6cc
s160419091 <s160419091@student.ubaya.ac.id>
2021-12-07 01:14:47 +0700

    custom

--- webservice/getHistory.php
+++ webservice/getHistory.php
@@ -4,7 +4,7 @@ require_once 'koneksi.php';
 
 extract($_POST);
 
-$sql = 'SELECT h.id, p.name as `location`, h.checkin, h.checkout,h.doses FROM histories h INNER JOIN users u on u.username = h.users_id INNER JOIN places p on h.places_id = p.id WHERE u.username = ?';
+$sql = 'SELECT h.id, p.name as `location`, h.checkin, h.checkout,h.doses FROM histories h INNER JOIN users u on u.id = h.users_id INNER JOIN places p on h.places_id = p.id WHERE u.username = ?';
 
 $stmt = $conn->prepare($sql);
 $stmt->bind_param('s', $username);
