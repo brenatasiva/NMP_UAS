cd7ba9db65fa9298c2624ff05d16cb02473e576d
s160419091 <s160419091@student.ubaya.ac.id>
2021-12-07 19:02:20 +0700

    dune

--- app/src/main/java/id/web/rpgfantasy/protectcare51/ProfileFragment.kt
+++ app/src/main/java/id/web/rpgfantasy/protectcare51/ProfileFragment.kt
@@ -3,13 +3,13 @@ package id.web.rpgfantasy.protectcare51
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
@@ -70,7 +70,7 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val fab = v?.findViewById<FloatingActionButton>(R.id.fab)
+        val fab = v?.findViewById<FloatingActionButton>(R.id.fabLogout)
         fab?.let {
             it.setOnClickListener {
                 val username = ""
@@ -83,6 +83,14 @@ class ProfileFragment : Fragment() {
                 activity?.startActivity(Intent(activity,LoginActivity::class.java))
                 activity?.finish()
                 }
+        }
+
+        fabHelp.setOnClickListener {
+            val sendIntent = Intent(Intent.ACTION_VIEW).apply {
+                this.data = Uri.parse("https://www.pedulilindungi.id/")
+            }
+            val shareIntent = Intent.createChooser(sendIntent, "Choose your browser")
+            startActivity(shareIntent)
         }
     }
 
